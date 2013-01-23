42717ec9aca86e9bd9280e4465558f05158bcf9b
Cameron Johnston <cameron@needle.com>
2013-01-22 18:19:24 -0700

    bump to 0.2.0

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "cameron@rootdown.net"
 license          "Apache 2.0"
 description      "Installs/Configures pypiserver"
 
-version          "0.1"
+version          "0.2.0"
 
 %w{git python runit}.each do |cb|
   depends cb
