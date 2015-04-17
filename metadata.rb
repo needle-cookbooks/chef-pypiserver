e82e79db3c8eeaf8d78063ec2dd53d954427ca4a
Damian Pantano <damian@needle.com>
2015-04-17 01:34:55 -0600

    Bump to 0.4.0 release

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "cameron@rootdown.net"
 license          "Apache 2.0"
 description      "Installs/Configures pypiserver"
 
-version          "0.2.0"
+version          "0.4.0"
 
 %w{git python runit}.each do |cb|
   depends cb
